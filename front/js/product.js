5fef3b99ed9827ce5ab2c54e9d87061e4999b40a
KeitaSandy <sandy.keita@hotmail.com>
2023-02-10 16:23:28 +0100

    localstorage

--- front/js/product.js
+++ front/js/product.js
@@ -20,7 +20,7 @@ function displayProduct(productChoice) {
   //insertion de l'image du produit
   image.src = productChoice.imageUrl;
   image.alt = productChoice.altTxt;
-  document.querySelector(".item__img").appendChild(image) 
+  document.querySelector(".item__img").appendChild(image);
   //nom du produit
   document.getElementById("title").innerText = productChoice.name;
   // prix du produit
@@ -28,14 +28,49 @@ function displayProduct(productChoice) {
   // description du produit
   document.getElementById("description").innerText = productChoice.description;
   // ajout des couleurs via une boucle
-productChoice.colors.forEach(function (color) {
-  const prodOption = document.createElement("option");
-  prodOption.value = color;
-  prodOption.innerText = color;
   const prodSelect = document.getElementById("colors");
-  prodSelect.appendChild(prodOption)
-});
-// ajout des produits dans le panier
+  for (let color of productChoice.colors) {
+    const prodOption = document.createElement("option");
+    prodOption.value = color;
+    prodOption.innerText = color;
+    prodSelect.appendChild(prodOption);
+  }
 
- 
+  // click sur le bouton
+  const button = document.getElementById("addtocart");
+
+  // création du bouton d'ajout au panier
+
+  let addToBasket = document.getElementById("addToCart");
+
+  //localstorage
+  let lSBasket = JSON.parse(localStorage.getItem("basket"));
+  addToBasket.addEventListener("click", () => {
+    //on recupere la couleur sélectionnée
+    let colorChoice= document.getElementById('colors').value;
+    // on recupère la quantité souhaitée
+    let quantityChoice = document.getElementById('quantity').value;
+    // on récupère l'ID du produit choisi
+    const id = idProduct;
+    // vérifier qu'il y a une couleur ET une quantité renseignés avant d'ajouter au panier (avec mini/maxi pour les quantités)
+    if (quantityChoice == 0 || quantityChoice > 100 || colorChoice == "") {
+      alert("Merci de préciser une quanité entre 1 et 100 et/ou une couleur");
+    } else{
+      // on cré un produit avec ces valeurs
+      let finalProduct = {
+        id: id,
+        name: productChoice.name,
+        img: productChoice.imageUrl,
+        color: colorChoice,
+        quantity: quantityChoice,       
+      }
+      console.log(finalProduct);
+      localStorage.setItem("finalProduct", JSON.stringify(finalProduct));
+    }
+    
+    
+     
+  });
+
+  
 }
